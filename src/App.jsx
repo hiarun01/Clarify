fde0f570ed2b70817282a93cc2f65893cecaf3d0
hiarun01 <hiarun02@gmail.com>
2025-04-04 13:04:05 +0530

    add required file and

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
-  return <>App</>;
+  return (
+    <div>
+      {/* Header */}
+      <header className=""> AI Image Enhancer</header>
+      <Home />
+      <footer>build bg : hiarun01</footer>
+    </div>
+  );
 }
 
 export default App;
