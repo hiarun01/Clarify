c8d9eb2c06957ddb83a24626be465f8433229183
hiarun01 <hiarun02@gmail.com>
2025-04-04 13:11:06 +0530

    header done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import "./App.css";
 import Home from "./components/Home";
+import {Image} from "lucide-react";
 
 function App() {
   return (
     <div>
       {/* Header */}
-      <header className=""> AI Image Enhancer</header>
+      <header className="bg-red-600 text-center text-white py-3 text-lg font-bold flex justify-center items-center gap-2">
+        <div>
+          <Image />
+        </div>
+        <div>
+          <span className="text-lg">AI Image Enhancer</span>
+        </div>
+      </header>
+      {/* Home */}
       <Home />
       <footer>build bg : hiarun01</footer>
     </div>
